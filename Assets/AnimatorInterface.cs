8b2df37ff476e11e1b4b496b92a9c5909078daa1
Sean Briggs <seanrgbriggs@gmail.com>
2018-01-27 01:39:51 -0500

    mememab

---
+++ Assets/AnimatorInterface.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorInterface : MonoBehaviour {
+
+    public MohawkBossImpl mhbImpl;
+
+	// Use this for initialization
+	void Start () {
+        mhbImpl = GetComponent<SBR.Brain>().activeController as MohawkBossImpl;
+	}
+	
+    public void NotifyEndAnim()
+    {
+        mhbImpl.endAnimationFlag = true;
+    }
+}
