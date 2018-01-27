b893f909b9ff85f5c4b067928a349a96c67c918f
Sean Briggs <seanrgbriggs@gmail.com>
2018-01-27 01:05:00 -0500

    Boss Logic

--- Assets/MohawkBossImpl.cs
+++ Assets/MohawkBossImpl.cs
@@ -5,29 +5,85 @@ public class MohawkBossImpl : MohawkBoss {
     
     public Transform player;
     public float chaseSpeed;
-    public float beginAttackRadius;
-    public float giveUpChaseTime;
-    public float giveUpChaseTimeVariance;
-    
+    public float beginChargeRadius;
+    public VariantFloat giveUpChaseTime;
+    public VariantFloat shotsShotsTime;
+    CooldownTimer giveUpChase;
+    CooldownTimer shotTimer;
 
-    protected override void State_Chase() {
-        Vector3 towardsPlayer = (player.position - transform.position);
-        transform.Translate(towardsPlayer.normalized * Mathf.Clamp(chaseSpeed, 0, towardsPlayer.magnitude) * Time.deltaTime, Space.World);
-        transform.rotation = Quaternion.LookRotation(towardsPlayer, Vector3.up);
+    public int chargesBeforeSweeping = 2;
+    int consecutiveCharges = 0;
+
+    public bool endAnimationFlag = false;
+
+    Vector3 towardsPlayer {
+        get {
+            return (player.position - transform.position);
+        }
     }
-    protected override void State_Leap() { }
-    protected override void StateEnter_ShotsShotsShots() { }
-    protected override void State_ShotsShotsShots() { }
-    protected override void State_Charge() { }
-    protected override void State_Sweep() { }
 
-    protected override bool TransitionCond_Chase_Leap() { return false; }
-    protected override bool TransitionCond_Chase_ShotsShotsShots() { return false; }
-    protected override bool TransitionCond_Chase_Charge() { return false; }
-    protected override bool TransitionCond_Chase_Sweep() { return false; }
-    protected override bool TransitionCond_Leap_Chase() { return false; }
-    protected override bool TransitionCond_ShotsShotsShots_Chase() { return false; }
-    protected override bool TransitionCond_Charge_Chase() { return false; }
-    protected override bool TransitionCond_Sweep_Chase() { return false; }
+    protected override void OnControllerEnabled()
+    {
+        base.OnControllerEnabled();
+        shotTimer = new CooldownTimer(shotsShotsTime.Evaluate());
+    }
+
+    void TriggerAnimation(string anim)
+    {
+        if (anim != "Charge" && anim != "Chase") { consecutiveCharges = 0; }
+        //Begin an animation in the appropriate state machine
+        print(anim);
+
+        endAnimationFlag = true;
+    }
+
+
+    protected override void StateEnter_Chase()
+    {
+        print("chase");
+        giveUpChase = new CooldownTimer(giveUpChaseTime.Evaluate());
+    }
+    protected override void State_Chase() {
+        transform.Translate(towardsPlayer.normalized * Mathf.Clamp(chaseSpeed, 0, towardsPlayer.magnitude) * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(towardsPlayer, Vector3.up), Vector3.up);
+    }
+    protected override void StateEnter_Leap() {
+        TriggerAnimation("Leap");
+    }
+    protected override void StateEnter_ShotsShotsShots() {
+        shotTimer = new CooldownTimer(shotsShotsTime.Evaluate());
+        TriggerAnimation("ShotsShotsShots");
+    }
+    protected override void StateEnter_Charge()
+    {
+        consecutiveCharges++;
+        TriggerAnimation("Charge");
+    }
+    protected override void StateEnter_Sweep()
+    {
+        TriggerAnimation("Sweep");
+    }
+
+    protected override bool TransitionCond_Chase_Leap() { return giveUpChase.Use(); }
+    protected override bool TransitionCond_Chase_ShotsShotsShots() { return shotTimer.Use(); }
+    protected override bool TransitionCond_Chase_Charge() {
+        return consecutiveCharges < chargesBeforeSweeping && towardsPlayer.magnitude < beginChargeRadius;
+    }
+    protected override bool TransitionCond_Chase_Sweep() { return consecutiveCharges >= chargesBeforeSweeping; }
+
+    protected override bool TransitionCond_Leap_Chase() { return ResetAnimFlag(); }
+    protected override bool TransitionCond_ShotsShotsShots_Chase() { return ResetAnimFlag(); }
+    protected override bool TransitionCond_Charge_Chase() { return ResetAnimFlag(); }
+    protected override bool TransitionCond_Sweep_Chase() { return ResetAnimFlag(); }
+
+    bool ResetAnimFlag()
+    {
+        if (endAnimationFlag)
+        {
+            endAnimationFlag = false;
+            return true;
+        }
+        return false;
+    }
 
 }
---
+++ Assets/VariantFloat.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VariantFloat{
+    public float baseValue;
+    public float variance;
+    public float Evaluate()
+    {
+        return baseValue + Random.Range(-variance, variance);
+    }
+}
