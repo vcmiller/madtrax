08e4a63d1fbed1555fe90b289df8c749f1cec1ca
Vincent Miller <vmiller0725@gmail.com>
2018-01-28 14:56:20 -0500

    menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+    public void LoadLevel(string level) {
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
